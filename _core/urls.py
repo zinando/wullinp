e26d0f3f20201c73bf3ee434ce1dd9d7dda66a78
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2025-02-02 07:12:54 +0100

    Create the home page app

--- _core/urls.py
+++ _core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home.views.home, name='home'),
 ]
