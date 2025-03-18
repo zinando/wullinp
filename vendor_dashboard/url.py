b92f56ad0d5ae30e03ad3ec28714f873912eed23
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2025-03-18 01:00:47 +0100

    Implement order template: pending orders

--- vendor_dashboard/url.py
+++ vendor_dashboard/url.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import vendor_dashboard
+from .views import vendor_dashboard, orders
 
 urlpatterns = [
     path('dashboard/', vendor_dashboard, name='vendor_dashboard'),
+    path('orders/', orders, name='orders'),
 ]
--- vendor_dashboard/views.py
+++ vendor_dashboard/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='vendor_login')
 def vendor_dashboard(request):
     return render(request, 'vendor_dashboard.html')
+
+
+# route for managing orders
+@login_required(login_url='vendor_login')
+def orders(request):
+    action = request.GET.get('action')
+    if action == 'get-pending-orders':
+        return render(request, 'orders/pending_orders.html')
+    return render(request, '404.html')
